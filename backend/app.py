0c00ef56d81a03d2de08ca16364eebc819493e13
MartinOravecSvK <oravec.martin01@gmail.com>
2024-05-29 20:25:06 +0100

    update

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from utils.data_functions import get_study_data 
+from utils.data_functions import get_study_data, update_elos
 
 # For testing purposes
 import random
@@ -9,6 +9,7 @@ import random
 app = Flask(__name__)
 CORS(app)
 app.secret_key = 'your_secret_key'
+number_of_questions = 2
 
 # Load the study data as pandas DataFrame
 study_data = get_study_data()
@@ -18,29 +19,30 @@ user_progress = {}
 
 def get_next_events(user_id):
     if user_id not in user_progress:
-        user_progress[user_id] = 0
+        user_progress[user_id] = [0, []]
 
     current_completed = user_progress[user_id]
 
     # Check if the user has completed all the questions
-    if current_completed >= len(study_data):
+    if current_completed >= number_of_questions:
         # Handle the completion of the study using empty list as flag
         return []
 
     # For now get 2 random even_details from study_data
-    next_events = []
-    for i in range(2):
-        # Get the next event
-        # For now, just get a random event
-        rand_id = random.randint(0, len(study_data) - 1)
-        next_event = study_data.iloc[rand_id]
-        next_events.append({
+    next_events = [study_data.iloc[random.randint(0, len(study_data) - 1)] for _ in range(2)]
+
+    user_progress[user_id][1] = [next_event['event_ID'] for next_event in next_events]
+
+    next_events_list = []
+    for i, next_event in enumerate(next_events):
+        next_events_list.append({
             f"event{i}_details": next_event['event_details'],
             f"event{i}_ID": next_event['event_ID']
         })
 
-    return next_events
+    return next_events_list
 
+# this route gets the next question for the user
 @app.route('/next', methods=['GET'])
 def get_next_question():
     user_id = request.args.get('user_id')
@@ -56,6 +58,7 @@ def get_next_question():
     
     return jsonify(next_events), 200
 
+
 @app.route('/submit', methods=['POST'])
 def submit_answer():
     # Get the user ID from the request
@@ -65,18 +68,35 @@ def submit_answer():
     if not user_id:
         return jsonify({"error": "User ID not provided"}), 400
 
-    data = request.json.get('answer')
-    if not data:
+    winner_id = request.json.get('winner_id')
+    if not winner_id:
         return jsonify({"error": "Answer not provided"}), 400
+    loser_id = user_progress[user_id][1][0] if user_progress[user_id][1][0] != winner_id else user_progress[user_id][1][1]
 
     # Handle the submitted answer here
-    print(f"Received answer from {user_id}: {data}")
+    print(f"Received answer from {user_id}: {winner_id}")
 
     # Update the user progress and ELO ratings
+    user_progress[user_id][0] += 1
+    winner_elo = study_data.loc[study_data['event_ID'] == winner_id, 'elo_rating']
+    loser_elo = study_data.loc[study_data['event_ID'] == loser_id, 'elo_rating']
 
+    # Update the ELO ratings
+    winner_new_elo, loser_new_elo = update_elos(winner_elo, loser_elo)
+
+    # Print the changes (event_IDs and ELO ratings new and old)
+    print(f"Winner: {winner_id}, Old ELO: {winner_elo.values[0]}, New ELO: {winner_new_elo}")
+    print(f"Loser: {loser_id}, Old ELO: {loser_elo.values[0]}, New ELO: {loser_new_elo}")
+
+    # Update the ELO ratings in the study data
+    study_data.loc[study_data['event_ID'] == winner_id, 'elo_rating'] = winner_new_elo
+    study_data.loc[study_data['event_ID'] == loser_id, 'elo_rating'] = loser_new_elo
 
     # Get the next questions
     next_events = get_next_events(user_id)
+
+    if next_events == []:
+        return jsonify({"message": "Study completed"}), 200
 
     return jsonify(next_events), 200
 
