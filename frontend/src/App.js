44a34e994647d19a8aa9f6c6ee922d250985efe6
MartinOravecSvK <oravec.martin01@gmail.com>
2024-05-31 10:14:42 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,74 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import StudyPage from './pages/StudyPage';
 
 function App() {
-    const [events, setEvents] = useState({});
-
-    // Replace 'some-unique-user-id' with a unique identifier for the user (generate some and store it in browser's local storage)
-    const userId = 'some-unique-user-id';
-
+    // This useEffect hook is used to generate a random 8-character user ID and store it in the browser's local storage
+    // This will be changed later to make sure there are no clashing user IDs
+    // For that a new backend API will be created to generate a unique user ID
     useEffect(() => {
-        fetchEvents();
+        // Check if user ID is already set in local storage
+        if (!localStorage.getItem('user_id')) {
+            // Generate a random 8-character user ID
+            const userId = 'user-' + Math.random().toString(36).substr(2, 8);
+            localStorage.setItem('user_id', userId);
+        }
     }, []);
-
-    const fetchEvents = async () => {
-        try {
-            const response = await fetch(`http://localhost:5000/next?user_id=${userId}`);
-            const data = await response.json();
-            if (data.events) {
-                setEvents(data.events);
-                console.log('New events fetched:', data.events)
-            } else {
-                console.log(data.error || data.message || 'No events found')
-                setEvents({});
-            }
-        } catch (error) {
-            console.error('Error fetching events:', error);
-        }
-    };
-
-    const submitAnswer = async (winnerId) => {
-        try {
-            const response = await fetch('http://localhost:5000/submit', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    user_id: userId,
-                    winner_id: winnerId,
-                }),
-            });
-            const data = await response.json();
-            if (data.events) {
-                setEvents(data.events);
-                console.log('New events fetched:', data.events)
-            } else {    
-                console.log(data.error || data.message || 'No events found')
-                setEvents({});
-            }
-        } catch (error) {
-            console.error('Error submitting answer:', error);
-        }
-    };
 
     return (
         <div className="App">
-            <h1>Choose the Event</h1>
-            {events != {} ? (
-                <div className="events">
-                    <div className="event">
-                        <h2>{events.event0_details}</h2>
-                        <button onClick={() => submitAnswer(events.event0_ID)}>Choose This Event</button>
-                    </div>
-                    <div className="event">
-                        <h2>{events.event1_details}</h2>
-                        <button onClick={() => submitAnswer(events.event1_ID)}>Choose This Event</button>
-                    </div>
-                </div>
-            ) : (
-                <p>No more events to show. Study completed!</p>
-            )}
+            <StudyPage />
         </div>
     );
 }
---
+++ frontend/src/pages/StudyPage.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import './StudyPage.css';
+
+function StudyPage() {
+    const [events, setEvents] = useState({});
+
+    // Replace 'some-unique-user-id' with a unique identifier for the user (generate some and store it in browser's local storage)
+    const userId = localStorage.getItem('user_id') || null;
+    console.log('User ID:', userId);
+
+    if (!userId) {
+        console.error('User ID not found in local storage');
+    }
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    const fetchEvents = async () => {
+        try {
+            const response = await fetch(`http://localhost:5000/next?user_id=${userId}`);
+            const data = await response.json();
+            if (data.events) {
+                setEvents(data.events);
+                console.log('New events fetched:', data.events)
+            } else {
+                console.log(data.error || data.message || 'No events found')
+                setEvents({});
+            }
+        } catch (error) {
+            console.error('Error fetching events:', error);
+        }
+    };
+
+    const submitAnswer = async (winnerId) => {
+        try {
+            const response = await fetch('http://localhost:5000/submit', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user_id: userId,
+                    winner_id: winnerId,
+                }),
+            });
+            const data = await response.json();
+            if (data.events) {
+                setEvents(data.events);
+                console.log('New events fetched:', data.events)
+            } else {    
+                console.log(data.error || data.message || 'No events found')
+                setEvents({});
+            }
+        } catch (error) {
+            console.error('Error submitting answer:', error);
+        }
+    };
+
+    return (
+        <div className="StudyPage">
+            <h1>Choose the Event</h1>
+            {events != {} ? (
+                <div className="events">
+                    <div className="event">
+                        <h2>{events.event0_details}</h2>
+                        <button onClick={() => submitAnswer(events.event0_ID)}>Choose This Event</button>
+                    </div>
+                    <div className="event">
+                        <h2>{events.event1_details}</h2>
+                        <button onClick={() => submitAnswer(events.event1_ID)}>Choose This Event</button>
+                    </div>
+                </div>
+            ) : (
+                <p>No more events to show. Study completed!</p>
+            )}
+        </div>
+    );
+}
+
+export default StudyPage;
