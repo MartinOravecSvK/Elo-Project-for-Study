61603abd0b3b1e04d42517a67036a98e6ab33ea2
MartinOravecSvK <oravec.martin01@gmail.com>
2024-05-31 11:37:38 +0100

    Added handling of participant ID sent when redirected from prolific

--- frontend/src/pages/FinishedStudyPage.js
+++ frontend/src/pages/FinishedStudyPage.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import './FinishedStudyPage.css';  // Ensure your CSS is being imported here
 
+// TODO:
+// - Fix broken Logo image path (there is none that is valid)
+// - Double check it works as intended
 
 function FinishedStudyPage() {
+
+    useEffect(() => {
+        const participantId = localStorage.getItem('participant_id');
+        if (participantId) {
+            // Redirect the participant after a delay
+            // For production, change the delay to 5000 (5 seconds)
+            setTimeout(() => {
+                window.location.href = `https://app.prolific.com/submissions/complete?cc=COBF1PK2&participant_id=${participantId}`;
+            }, 500000);
+        } else {
+            // If participant ID is not found, give them an option to input it manually if it makes sense
+        }
+    }, []);
+
     // Tried to integrate the original code for the finished study page into react as best as possible
     return (
         <div className="jspsych-display-element">
             {/* Using Helmet instead of Head as you can't normally directly change head of the html page in react thus we use a  library react-helmet*/}
             <Helmet>
                 <title>End Study</title>
-                <meta http-equiv="refresh" content="5; url=https://app.prolific.com/submissions/complete?cc=COBF1PK2" />
+                {/* <meta http-equiv="refresh" content="5; url=https://app.prolific.com/submissions/complete?cc=COBF1PK2" /> */}
                 <link href="../../jspsych-6.2.0/css/jspsych.css" rel="stylesheet" type="text/css" />
             </Helmet>
             <div className="jspsych-content-wrapper">
@@ -22,7 +39,9 @@ function FinishedStudyPage() {
                             alt="Logo"
                         />
                     </div>
-                    <h2><b>THANK YOU FOR COMPLETING TODAY'S STUDY</b></h2>
+                    <h2><b>
+                        THANK YOU FOR COMPLETING TODAY'S STUDY
+                    </b></h2>
                     <p>
                         Please note: If you experience any adverse effects to your mental health please seek help from your
                         usual care providers. If you require immediate help, online advice can be found at{' '}
@@ -32,10 +51,14 @@ function FinishedStudyPage() {
                         , or alternatively you can contact a Samaritan free of charge on 116 123 (available 24 hours a day,
                         365 days a year).
                     </p>
-                    <p><b>Please click the link below to submit your data and return to Prolific</b></p>
+                    <p><b>
+                        Please click the link below to submit your data and return to Prolific
+                    </b></p>
                     <p>
                         <b>
-                            <a href="https://app.prolific.com/submissions/complete?cc=COBF1PK2">Click here to finish study</a>
+                            <a href={`https://app.prolific.com/submissions/complete?cc=COBF1PK2&participant_id=${localStorage.getItem('participant_id')}`} >
+                                Click here to finish study
+                            </a>
                         </b>
                     </p>
                     <p>
